10e9debf17e70110c3171bcadbf3cc6279bd3ef0
Athmekr <athmekr@gmail.com>
2023-02-18 03:27:55 +0200

    Added Actions column with edit and delete. Added onDelete function with sweet alert. Created router links

--- frontend/src/app/components/employees-view/employees-view.component.ts
+++ frontend/src/app/components/employees-view/employees-view.component.ts
@@ -1,9 +1,11 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { EmployeeService } from "../../services/employee.service";
 import { Employee } from "../../interfaces/employee.interface";
+import swal from "sweetalert2";
+
 
 
 @Component({
@@ -13,7 +15,7 @@ import { Employee } from "../../interfaces/employee.interface";
 })
 export class EmployeesViewComponent implements AfterViewInit {
 
-  displayedColumns: string[] = ['firstname', 'surname', 'hiringDate', 'skills', 'createdAt', 'updatedAt'];
+  displayedColumns: string[] = ['firstname', 'surname', 'hiringDate', 'skills', 'createdAt', 'updatedAt', 'actions'];
   dataSource: MatTableDataSource<Employee>;
   public employees: Employee[];
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -42,8 +44,32 @@ export class EmployeesViewComponent implements AfterViewInit {
       });
       this.dataSource = new MatTableDataSource(this.employees);
     });
-
   }
+
+  onDelete(employee: Employee){
+    swal.fire({
+      title: `Are you sure you want to delete employee ${employee.firstname + ' ' + employee.surname}?`,
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#f44336',
+      cancelButtonColor: '#919191',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.employeeService.deleteEmployee(employee._id)
+          .subscribe( () => {
+            this.getSkills();
+          });
+        swal.fire(
+          'Deleted!',
+          'Employee has been deleted.',
+          'success'
+        )
+      }
+    })
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
