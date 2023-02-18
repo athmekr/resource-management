a4f840727f6f3b2a5b402e2bef6e2f6196ef252c
Athmekr <athmekr@gmail.com>
2023-02-18 17:37:56 +0200

    Created getEmployee, updateEmployee, createEmployee endpoints

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -13,6 +13,18 @@ export class EmployeeService{
     return this.httpClient.get<Employee[]>(`${this.url}/employees/get`);
   }
 
+  getEmployee(employeeId: string) {
+    return this.httpClient.get<Employee>(`${this.url}/employees/get/${employeeId}`);
+  }
+
+  updateEmployee(employee: Employee) {
+    return this.httpClient.patch<Employee>(`${this.url}/employees/update/${employee._id}`, employee);
+  }
+
+  createEmployee(employee: Employee){
+    return this.httpClient.post<Employee>(`${this.url}/employees/create`, employee);
+  }
+
   deleteEmployee(employeeId: string) {
     return this.httpClient.delete<Employee>(`${this.url}/employees/delete/${employeeId}`);
   }
